5669e6490f119bbd0c667bff47bc5265346d8158
gayeong <lgy0530@naver.com>
2023-07-14 14:49:46 +0900

    Initial Commit

---
+++ AlertModule/AlertModule.h
@@ -0,0 +1,18 @@
+//
+//  AlertModule.h
+//  AlertModule
+//
+//  Created by 애니모비 on 2023/07/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlertModule.
+FOUNDATION_EXPORT double AlertModuleVersionNumber;
+
+//! Project version string for AlertModule.
+FOUNDATION_EXPORT const unsigned char AlertModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertModule/PublicHeader.h>
+
+
