a826e7203084231ce89c2ae156b1a9d4f99b1268
gayeong <lgy0530@naver.com>
2023-07-20 14:55:25 +0900

    [FIX] AlertConstants.swift - color extension 제거 (외부에서 변경 불가능하므로)

---
+++ AlertModule/Settings/AlertConstants.swift
@@ -0,0 +1,17 @@
+//
+//  AlertConstants.swift
+//  AlertModule
+//
+//  Created by 애니모비 on 2023/07/14.
+//
+
+import Foundation
+import SwiftUI
+
+public enum AlertLayout {
+    public static let BACKGROUND_FRAME_WIDTH_SIZE: CGFloat = 320
+    public static let BACKGROUND_CORNER_RADIUS: CGFloat = 10
+    public static let BUTTON_CORNER_RADIUS: CGFloat = 5
+    public static let BUTTON_TEXT_SIZE: CGFloat = 16
+    public static let MESSAGE_TEXT_SIZE: CGFloat = 16
+}
