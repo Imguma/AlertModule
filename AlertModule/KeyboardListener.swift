8be5717cd1ee8d64993ac5a43a5a9961068e4cdf
gayeong <lgy0530@naver.com>
2023-07-20 14:51:26 +0900

    [FEAT] KeyboardListener.swift - custom alert input 창 활성시 alert view가 이동하지 않아 키보드 올라옴 감지 리스너 생성

---
+++ AlertModule/KeyboardListener.swift
@@ -0,0 +1,28 @@
+//
+//  KeyboardListener.swift
+//  AlertModule
+//
+//  Created by 애니모비 on 2023/07/20.
+//
+
+import Combine
+import UIKit
+
+protocol KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> { get }
+}
+
+extension KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .map { _ in true },
+            
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in false }
+        )
+        .eraseToAnyPublisher()
+    }
+}
